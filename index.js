221b177dfd268ee2d454c75574bad43e8a575e3d
Juani Pijuan <juanipijuan@gmail.com>
2024-04-15 21:56:35 -0300

    primera entrega js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (!fishTypes.includes(fishType)) {
     const price = prices[index] * quantity;
     cartItems.push({ type: fishType, quantity: quantity, price: price });
     totalPrice += price;
-    alert(`¡${quantity}x ${fishType} ha sido añadido al carrito!\nTotal: ${totalPrice} €`);
+    alert(`¡${quantity}x ${fishType} ha sido añadido al carrito!\nTotal: ${totalPrice} $`);
   }
 }
 
@@ -29,8 +29,8 @@ if (cartItems.length === 0) {
 } else {
   let cartSummary = "Contenido del carrito:\n";
   cartItems.forEach(item => {
-    cartSummary += `${item.quantity}x ${item.type} - ${item.price} €\n`;
+    cartSummary += `${item.quantity}x ${item.type} - ${item.price} $\n`;
   });
-  cartSummary += `Total: ${totalPrice} €`;
+  cartSummary += `Total: ${totalPrice} $`;
   alert(cartSummary);
 }
